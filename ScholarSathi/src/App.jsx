01b65e51f26caa809573c7193e18ddba4cc423c0
sudip <chsudip30@gmail.com>
2025-06-09 22:33:42 +0545

    Initial commit

---
+++ ScholarSathi/src/App.jsx
@@ -0,0 +1,12 @@
+import RegisterForm from './Pages/RegisterForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default App;
