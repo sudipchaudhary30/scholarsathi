a887aa2c5d293e5414a0b761242f255eb1171a63
nilisha <nilishakhadgi29@gmail.com>
2025-06-30 12:15:19 +0545

    App.jsx

--- ScholarSathi/src/App.jsx
+++ ScholarSathi/src/App.jsx
@@ -1,11 +1,30 @@
-import RegisterForm from './Pages/RegisterForm';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Dashboard from './Pages/Dashboard';
+import ManageMascots from './Pages/ManageMascots';
+import AssignLocations from './Pages/AssignLocations';
+import ScholarshipOffers from './Pages/ScholarshipOffers';
+import StudentData from './Pages/StudentData';
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <RegisterForm />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />          
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/manage-mascots" element={<ManageMascots />} />
+        <Route path="assign-locations" element={<AssignLocations />} />
+        <Route path="/scholarship-offers" element={<ScholarshipOffers />} />
+        <Route path="/student-data" element={<StudentData />} />
+
+
+      
+      </Routes>
+    </Router>
   );
 }
 
