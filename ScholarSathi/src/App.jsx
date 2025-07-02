192bdab1d8fbc838c059f8cb697ff31ccb642c8d
oshikadahal <149761380+oshikadahal@users.noreply.github.com>
2025-07-02 11:28:02 +0545

    User panel updated

--- ScholarSathi/src/App.jsx
+++ ScholarSathi/src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import LandingPage from './Pages/landingpage';
-import RegisterForm from './Pages/RegisterForm';
+
 import ContactUs from './Pages/ContactUs'; 
 import PatnerUni from './Pages/PatnerUni'; 
 import AboutUs from './Pages/AboutUs'; // <-- Add this line
@@ -18,7 +18,7 @@ function App() {
       <div className="main-content">
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/register" element={<RegisterForm />} />
+          
           <Route path="/contact-us" element={<ContactUs />} /> 
            <Route path="/partner-universities" element={<PatnerUni />} />
            <Route path="/about-us" element={<AboutUs />} />
